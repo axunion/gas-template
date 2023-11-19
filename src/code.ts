6674340987df6017f4fdf63f24a04af1513828c5
axunion <120321953+axunion@users.noreply.github.com>
2023-11-19 23:36:37 +0900

    Create Project

---
+++ src/code.ts
@@ -0,0 +1,4 @@
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+function greeter(person: string) {
+  return `Hello, ${person}!`;
+}
---
+++ src/test.ts
@@ -0,0 +1,5 @@
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+function test() {
+  const user = "Grant";
+  Logger.log(greeter(user));
+}
