d4846b3da590c5aaea2dec2efbaf580d777632f4
axunion <120321953+axunion@users.noreply.github.com>
2023-11-30 20:45:54 +0900

    Update default values

---
+++ src/config.ts
@@ -0,0 +1,4 @@
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+const config = {
+  ID: "1",
+};
--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,4 @@
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 function greeter(person: string) {
-  return `Hello, ${person}!`;
+  return `${config.ID} : Hello, ${person}!`;
 }
