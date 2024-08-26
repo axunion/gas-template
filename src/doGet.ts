ead0ae2485a0911861691e9307427ce9c1dc5734
axunion <120321953+axunion@users.noreply.github.com>
2024-08-27 00:10:22 +0900

    Refine test output to be more informative

--- src/doGet.ts
+++ src/doGet.ts
@@ -8,7 +8,7 @@ type GetResponse = {
 function _doGet() {
   const e = { parameter: { type: "0" } };
   const result = doGet(e as unknown as GoogleAppsScript.Events.DoGet);
-  console.log(result);
+  console.log(result.getContent());
 }
 
 function doGet(
