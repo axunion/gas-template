abc4d5c00db1b0f120f6ad014788a288b93c9c2a
axunion <120321953+axunion@users.noreply.github.com>
2024-12-31 23:06:29 +0900

    Refactor codebase to align with Biome rules

--- src/doGet.ts
+++ src/doGet.ts
@@ -1,33 +1,32 @@
 type GetResponse = {
-  result: "done" | "error";
-  data?: unknown;
-  error?: string;
+	result: "done" | "error";
+	data?: unknown;
+	error?: string;
 };
 
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
 function _doGet() {
-  const e = { parameter: { type: "" } };
-  const result = doGet(e as unknown as GoogleAppsScript.Events.DoGet);
-  console.log(result.getContent());
+	const e = { parameter: { type: "" } };
+	const result = doGet(e as unknown as GoogleAppsScript.Events.DoGet);
+	console.log(result.getContent());
 }
 
 function doGet(
-  e: GoogleAppsScript.Events.DoGet,
+	e: GoogleAppsScript.Events.DoGet,
 ): GoogleAppsScript.Content.TextOutput {
-  const response: GetResponse = { result: "done" };
+	const response: GetResponse = { result: "done" };
 
-  try {
-    const type = e.parameter.type;
+	try {
+		const type = e.parameter.type;
 
-    if (!type) {
-      throw new Error(`Invalid parameter.`);
-    }
+		if (!type) {
+			throw new Error("Invalid parameter.");
+		}
 
-    response.data = type;
-  } catch (error) {
-    response.result = "error";
-    response.error = error.message;
-  }
+		response.data = type;
+	} catch (error) {
+		response.result = "error";
+		response.error = error.message;
+	}
 
-  return ContentService.createTextOutput(JSON.stringify(response));
+	return ContentService.createTextOutput(JSON.stringify(response));
 }
--- src/doPost.ts
+++ src/doPost.ts
@@ -1,25 +1,24 @@
 type PostResponse = {
-  result: "done" | "error";
-  error?: string;
+	result: "done" | "error";
+	error?: string;
 };
 
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
 function doPost(
-  e: GoogleAppsScript.Events.DoPost,
+	e: GoogleAppsScript.Events.DoPost,
 ): GoogleAppsScript.Content.TextOutput {
-  const response: PostResponse = { result: "done" };
+	const response: PostResponse = { result: "done" };
 
-  try {
-    const parameter = JSON.parse(e.postData.contents);
-    const type = parameter.type;
+	try {
+		const parameter = JSON.parse(e.postData.contents);
+		const type = parameter.type;
 
-    if (!type) {
-      throw new Error(`Invalid parameter.`);
-    }
-  } catch (error) {
-    response.result = "error";
-    response.error = error.message;
-  }
+		if (!type) {
+			throw new Error("Invalid parameter.");
+		}
+	} catch (error) {
+		response.result = "error";
+		response.error = error.message;
+	}
 
-  return ContentService.createTextOutput(JSON.stringify(response));
+	return ContentService.createTextOutput(JSON.stringify(response));
 }
